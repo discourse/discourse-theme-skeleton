5ff4d0d9197a3f7167825711400addab221eff0b
David Taylor <david@taylorhq.com>
2024-04-03 09:46:50 +0100

    Ensure initializer is functional on first install (#19)

--- javascripts/discourse/api-initializers/todo.js
+++ javascripts/discourse/api-initializers/todo.js
@@ -1,5 +1,5 @@
-// import { apiInitializer } from "discourse/lib/api";
+import { apiInitializer } from "discourse/lib/api";
 
-// export default apiInitializer("1.8.0", (api) => {
-//   console.log("hello world from api initializer!");
-// });
+export default apiInitializer("1.8.0", (/* api */) => {
+  // console.log("hello world from api initializer!");
+});
